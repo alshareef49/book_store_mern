87374ac9de9078cfc9029d0ae8e96688becd478c
alshareef49 <alshareef4039@gmail.com>
2023-10-16 21:52:25 +0530

    🚀 Added MongoDB database for enhanced data storage 📦

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT,mongoDBUrl } from "./config.js"
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(PORT,()=>{
-    console.log(`App is listening on PORT ${PORT}`);
-});
+
+app.get("/",(request,response)=>{
+    console.log(request);
+    return response.status(200).send('Welcome to mern stack tutorial');
+})
+
+// app.listen(PORT,()=>{
+//     console.log(`App is listening on PORT ${PORT}`);
+// });
+
+mongoose
+    .connect(mongoDBUrl)
+    .then(()=>{
+        console.log('App connected to database');
+        app.listen(PORT,()=>{
+            console.log(`App is listening on PORT ${PORT}`);
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
