86c4fca45c7ddff5607c0bb23a380be3702a1003
alshareef49 <alshareef4039@gmail.com>
2023-10-17 22:25:29 +0530

    📚🛠️ Implemented PUT request for updating book details 📖🔄

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { PORT,mongoDBUrl } from "./config.js"
+import express, { request, response } from "express";
+import { PORT, mongoDBUrl } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 
@@ -7,53 +7,100 @@ const app = express();
 //middleware for parsing json
 app.use(express.json());
 
-app.get("/",(request,response)=>{
-    console.log(request);
-    return response.status(200).send('Welcome to mern stack tutorial');
-})
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Welcome to mern stack tutorial");
+});
 
 //post request to add book to database
-app.post('/books',async (request,response)=>{
-    try {
-        if(!request.body.title || !request.body.author || !request.body.publishYear){
-            return response.status(400).send({message:'Send all required fields: title,author and publishYear'});
-        }
-        const newBook = {
-            title:request.body.title,
-            author:request.body.author,
-            publishYear:request.body.publishYear
-        }
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
-        
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({message:error.message});
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({
+          message: "Send all required fields: title,author and publishYear",
+        });
     }
-})
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
 
 //get request to get all books from database
-app.get('/books', async (request,response)=>{
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count:books.length,
-            data:books
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+//get request to get book by id from database
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// update request
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({
+          message: "Send all required fields: title,author and publishYear",
         });
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({message:error.message});
     }
-})
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id,request.body);
+
+    if(!result){
+        return response.status(404).send({message:'book not found'})
+    }
+    return response.status(200).send({message:'book updated successfully'})
+
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
-    .connect(mongoDBUrl)
-    .then(()=>{
-        console.log('App connected to database');
-        app.listen(PORT,()=>{
-            console.log(`App is listening on PORT ${PORT}`);
-        });
-    })
-    .catch((error)=>{
-        console.log(error);
+  .connect(mongoDBUrl)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening on PORT ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
