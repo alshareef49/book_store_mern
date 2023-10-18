a68a61edf0b7381f9c4187304fb4ef5283fa0f13
alshareef49 <alshareef4039@gmail.com>
2023-10-18 21:10:57 +0530

    🌐 Implemented CORS policy for enhanced security 🛡️

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ import express, { request, response } from "express";
 import { PORT, mongoDBUrl } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 //middleware for parsing json
 app.use(express.json());
+
+//middleware for handling cors POLICY
+//method 1: Allow all origin with default of cors(*)
+// app.use(cors());
+//method2: Allow custom Origin
+app.use(cors({
+  origin:'http"//localhost:3000',
+  methods:['GET','POST','PUT','DELETE'],
+  allowedHeaders:['Content-Type']
+}))
 
 app.get("/", (request, response) => {
   console.log(request);
